2732bbdf4bcb1b426fea7a4b38f7707b31010c23
ridwanfatchurrachim <163948623+ridwanfatchurrachim@users.noreply.github.com>
2024-03-19 11:28:43 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-var inputan=document.getElementBYId('inputan');
+var inputan=document.getElementById('inputan');
 	function tombol(obj){
 		var kondisi = obj.innerHTML;
-		if(kondisi =="="){
-			inputan.innerHTML= eval (inputan.innerHTML);
-		}else if(kondisi =="Del"){
+		if(kondisi=="="){
+			inputan.innerHTML = eval (inputan.innerHTML);
+		}else if(kondisi=="Del"){
 			inputan.innerHTML = inputan.innerHTML.slice(0,-1)||'0';
-		}else if(kondisi =="AC"){
+		}else if(kondisi=="AC"){
 			inputan.innerHTML ="0";
 		}else{
-			if(inputan.innerHTML =="0"){
+			if(inputan.innerHTML=="0"){
 				inputan.innerHTML = kondisi;
 		}else{
 			inputan.innerHTML += kondisi;
