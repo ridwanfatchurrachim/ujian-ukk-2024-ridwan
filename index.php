d09cf63a08e3671257694574e818a830f2659c7a
ridwanfatchurrachim <163948623+ridwanfatchurrachim@users.noreply.github.com>
2024-03-19 11:32:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css"hef="./css/style.css">
-	<title>Apliasi kalkulator</title>title>
+	<link rel="stylesheet" type="text/css"href="./css/style.css">
+	<title>Apliasi kalkulator</title>
 </head>
 <body>
-.
 <div class="kotak">
-	<table borader="1"Cellspacing="0">
+	<table border="1"cellspacing="0">
 		<tr>
-			<td colspan="2"><buton onclick="tombol(this)">AC</buton></td>
-			<td><button onclick="tombol(this)">Del</button>
-			</td>
+			<td colspan="4"id="inputan">0</td>
+		</tr>
+		<tr>
+			<td colspan="2"><button onclick="tombol(this)">AC</buton></td>
+			<td><button onclick="tombol(this)">Del</button></td>
 			<td><button onclick="tombol(this)">/</button></td>
 		</tr>
 		<tr>
@@ -35,10 +36,10 @@
 		<tr>
 			<td colspan="2"><button onclick="tombol(this)">0</button></td>
 			<td><button onclick="tombol(this)">.</button></td>
-			<td><button onclick="tombol(this)">=<</button></td>
+			<td><button onclick="tombol(this)">=</button></td>
 		</tr>
 	</table>
 </div>
-<scipt type="text/Javascript"src="./Js?scripts.js</script>
+<script type="text/javascript" src="./js/scripts.js"></script>
 </body>
 </html>
