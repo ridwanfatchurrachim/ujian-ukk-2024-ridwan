3dfacf6b6b0b6e7bf4aab4256af5384d872efbce
ridwanfatchurrachim <163948623+ridwanfatchurrachim@users.noreply.github.com>
2024-03-19 11:31:16 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var inputan=document.getElementById('inputan');
+	function tombol(obj){
+		var kondisi = obj.innerHTML;
+		if(kondisi=="="){
+			inputan.innerHTML = eval (inputan.innerHTML);
+		}else if(kondisi=="Del"){
+			inputan.innerHTML = inputan.innerHTML.slice(0,-1)||'0';
+		}else if(kondisi=="AC"){
+			inputan.innerHTML ="0";
+		}else{
+			if(inputan.innerHTML=="0"){
+				inputan.innerHTML = kondisi;
+		}else{
+			inputan.innerHTML += kondisi;
+		}
+	}
+}
